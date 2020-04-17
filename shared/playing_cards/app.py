678b8617691bc073e44417355aa79584e4689c8b
Shorya Malani <shorya@malanifamily.private>
2020-04-17 15:37:11 -0400

    starting to create a framework for users

--- shared/playing_cards/app.py
+++ shared/playing_cards/app.py
@@ -1,21 +1,38 @@
-from flask import Flask,render_template,jsonify
-from flask_login import current_user
+# DEPENDENCIES
+import random
+from flask import Flask,render_template,jsonify,session
 from flask_socketio import SocketIO,emit,join_room,leave_room
-async_mode = None
 
+#APP STUFF
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
+async_mode = None
 
+class user:
+    def __init__(self,id):
+        self.user_id = id
+    def set_name(self,name):
+        self.name = name 
+#ROUTERS
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
+#SOCKET HANDLERS
 @socketio.on('send_data')
 def send_back(data):
     emit("send_data_back",{
         "data":data["data_sent"]
     },broadcast=True)
+
+# This is for if we ever need ids. i accidentally made it not realizing we don't need it and can just use session.
+# @socketio.on('get_id')
+# def get_id():
+#     id = random.randint(10000000,999999999)
+#     this_user = user(id)
+#     session["user"] = this_user
+#     emit("send_id",{"id":id})
 
 if __name__ == '__main__':
     socketio.run(app,host='0.0.0.0',debug=True)
--- shared/playing_cards/static/script.js
+++ shared/playing_cards/static/script.js
@@ -1,16 +1,34 @@
-//GLOBALS
+// GLOBALS
 const socket = io()
+var client = {
+
+}
+
+//FUNCTIONS FOR FOR WORKING
+// function get_id() {
+//     var get_id = window.setInterval(function() {
+//         if (!client["id"]) {
+//             socket.emit("get_id")
+//         }
+//     }, 3000)
+// }
 
 function send_data() {
     const input = document.getElementById("testing_inputs").value
     socket.emit('send_data', { "data_sent": input })
 }
+
+// SOCKET HANDLERS
 socket.on('send_data_back', function(data) {
     document.getElementById("receive").textContent = data["data"]
 })
 
+// socket.on('set_id', function(data) {
+//     client["id"] = data
+// })
+
 function main() {
     //THIS IS WHERE ALL THE CODE WILL GO TO START FUNCTIONS
-
+    get_id()
 }
 main()
