ae31c414f12be6612f9709615b61ad58cf7ee44e
Shorya Malani <shorya@malanifamily.private>
2020-04-17 11:11:48 -0400

    model working of playing cards

--- shared/playing_cards/app.py
+++ shared/playing_cards/app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
+from flask_login import current_user
 from flask_socketio import SocketIO,emit,join_room,leave_room
 async_mode = None
 
@@ -10,7 +11,11 @@ socketio = SocketIO(app, async_mode=async_mode)
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-@socket.̨
+@socketio.on('send_data')
+def send_back(data):
+    emit("send_data_back",{
+        "data":data["data_sent"]
+    })
 
 if __name__ == '__main__':
     socketio.run(app,debug=True)
--- shared/playing_cards/static/script.js
+++ shared/playing_cards/static/script.js
@@ -1,10 +1,16 @@
-function set_up_socket() {
-    const socket = io()
-    return socket
-}
+//GLOBALS
+const socket = io()
 
+function send_data() {
+    const input = document.getElementById("testing_inputs").value
+    socket.emit('send_data', { "data_sent": input })
+}
+socket.on('send_data_back', function(data) {
+    document.getElementById("receive").textContent = data["data"]
+})
 
 function main() {
-    socket = set_up_socket()
+    //THIS IS WHERE ALL THE CODE WILL GO TO START FUNCTIONS
+
 }
 main()
