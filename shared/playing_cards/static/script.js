63a24ba8f69d1a1c2561fc6498cbb1d844e8c591
Shorya Malani <github-sho@smlist.eml.cc>
2020-04-18 16:05:16 -0400

    adding start_game

--- shared/playing_cards/static/script.js
+++ shared/playing_cards/static/script.js
@@ -65,7 +65,10 @@ socket.on('get_rooms', function(data) {
         room_div.appendChild(button)
     }
 })
-
+socket.on('start_game', function(data) {
+    document.getElementById("setup_rooms").hidden = true
+    document.getElementById("pick_game").hidden = false
+})
 
 function main() {
     //THIS IS WHERE ALL THE CODE WILL GO TO START FUNCTIONS
