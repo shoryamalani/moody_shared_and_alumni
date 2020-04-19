7e4840119d66126df964afd06f67a584461de888
Shorya Malani <github-sho@smlist.eml.cc>
2020-04-19 00:24:30 -0400

    tried g and a bunch of other things but ended up going back to the start

--- shared/playing_cards/app.py
+++ shared/playing_cards/app.py
@@ -1,6 +1,6 @@
 # DEPENDENCIES
 import random
-from flask import Flask,render_template,jsonify,session
+from flask import Flask,render_template,jsonify,session,g
 from flask_socketio import SocketIO,emit,join_room,leave_room,send
 import james_bond
 import blackjack
@@ -55,7 +55,7 @@ def create_room(data):
         session["room"] = client_room
         emit("add_room",{"name":name},broadcast=True)
         rooms["rooms"].append(name)
-        rooms[room] = client_room
+        rooms[name] = client_room
         emit("start_game",{"name":name})
         join_room(name)
     else:
