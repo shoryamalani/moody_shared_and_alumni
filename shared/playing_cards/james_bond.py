ab96ae63c2621b19e84b4c1f1541798c45ad914e
Shorya Malani <github-sho@smlist.eml.cc>
2020-04-21 16:49:16 -0400

    setting up the file for james bond. this can be used as a template when im done with it for other games

---
+++ shared/playing_cards/james_bond.py
@@ -0,0 +1,13 @@
+import random
+
+def interpreter(data):
+    if data["action"] == "start_game":
+        start_game(data["players"])
+
+def start_game(players):
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
